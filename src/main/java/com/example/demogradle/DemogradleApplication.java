84fb3c6a281f4537859da1d2c92bdaaee12b0fec
harpreetdawar <harpreetdawar@yahoo.com>
2024-04-17 07:28:02 -0600

    First Commit

---
+++ src/main/java/com/example/demogradle/DemogradleApplication.java
@@ -0,0 +1,20 @@
+package com.example.demogradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemogradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemogradleApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
+    }
+}
